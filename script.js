ac83952925ffeb279b4a8160b59b014bd8134fcc
bohara_ranjeev <ranjeevbohara9048@gmail.com>
2022-04-30 21:12:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const clos = document.getElementById('close');
+
+if(bar){
+    bar.addEventListener('click',()=>{
+      nav.classList.add('active');
+    })
+}
+
+if(clos){
+    clos.addEventListener('click',()=>{
+      nav.classList.remove('active');
+    })
+}
+
+
